cbc9f1a6c7fed12e15336b2c863e1f8df62a997e
Evgeniy Volodin <volodin1981@yandex.ru>
2023-07-31 16:02:12 +0300

    @222

---
+++ lesson55/src/notebook/model/repository/impl/UserRepository.java
@@ -0,0 +1,159 @@
+package notebook.model.repository.impl;
+
+// import notebook.model.dao.impl.FileOperation;
+import notebook.util.mapper.impl.UserMapper;
+import notebook.model.User;
+import notebook.model.repository.GBRepository;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+// import java.sql.SQLOutput;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class UserRepository implements GBRepository {
+    private final UserMapper mapper;
+    // private final FileOperation operation;
+    private final String fileName;
+
+    public UserRepository(String fileName) {
+        this.mapper = new UserMapper();
+        this.fileName = fileName;
+        try (FileWriter writer = new FileWriter(fileName, true)) {
+            writer.flush();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    @Override
+    public List<User> findAll() {
+        List<String> lines = readAll();
+        List<User> users = new ArrayList<>();
+        for (String line : lines) {
+            users.add(mapper.toOutput(line));
+        }
+        return users;
+    }
+
+    @Override
+    public User create(User user) {
+        List<User> users = findAll();
+        long max = 0L;
+        for (User u : users) {
+            long id = u.getId();
+            if (max < id) {
+                max = id;
+            }
+        }
+        long next = max + 1;
+        user.setId(next);
+        users.add(user);
+        write(users);
+        return user;
+    }
+
+    @Override
+    public Optional<User> findById(Long userId) {
+        List<User> users = findAll();
+        return users.stream()
+                .filter(u -> u.getId()
+                        .equals(userId))
+                .findFirst();
+    }
+
+    @Override
+    public Optional<User> update(Long userId, User update) {
+        List<User> users = findAll();
+        User editUser = users.stream()
+                .filter(u -> u.getId()
+                        .equals(userId))
+                .findFirst().orElseThrow(() -> new RuntimeException("User not found"));
+
+        if (!update.getFirstName().isEmpty())
+            editUser.setFirstName(update.getFirstName());
+        else
+            editUser.setFirstName(editUser.getFirstName());
+
+        if (!update.getLastName().isEmpty())
+            editUser.setFirstName(update.getLastName());
+        else
+            editUser.setLastName(editUser.getLastName());
+
+        if (!update.getPhone().isEmpty())
+            editUser.setPhone(update.getPhone());
+        else
+            editUser.setPhone(editUser.getPhone());
+
+        // editUser.setLastName(update.getLastName());
+        // editUser.setPhone(update.getPhone());
+        write(users);
+        return Optional.of(update);
+    }
+
+    @Override
+    public boolean delete(Long userId) {
+       List<User> users = findAll();
+        User editUser = users.stream()
+                .filter(u -> u.getId()
+                        .equals(userId))
+                .findFirst().orElseThrow(() -> new RuntimeException("User not found"));
+        editUser.deleteId(userId);
+        return true;
+    }    
+
+    private void write(List<User> users) {
+        List<String> lines = new ArrayList<>();
+        for (User u : users) {
+            lines.add(mapper.toInput(u));
+        }
+        saveAll(lines);
+    }
+
+    // @Override
+    public List<String> readAll() {
+        List<String> lines = new ArrayList<>();
+        try {
+            File file = new File(fileName);
+            // создаем объект FileReader для объекта File
+            FileReader fr = new FileReader(file);
+            // создаем BufferedReader с существующего FileReader для построчного считывания
+            BufferedReader reader = new BufferedReader(fr);
+            // считаем сначала первую строку
+            String line = reader.readLine();
+            if (line != null) {
+                lines.add(line);
+            }
+            while (line != null) {
+                // считываем остальные строки в цикле
+                line = reader.readLine();
+                if (line != null) {
+                    lines.add(line);
+                }
+            }
+            fr.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return lines;
+    }
+
+    // @Override
+    public void saveAll(List<String> data) {
+        try (FileWriter writer = new FileWriter(fileName, false)) {
+            for (String line : data) {
+                // запись всей строки
+                writer.write(line);
+                // запись по символам
+                writer.append('\n');
+            }
+            writer.flush();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
---
+++ lesson55/src/notebook/util/Commands.java
@@ -0,0 +1,11 @@
+package notebook.util;
+
+public enum Commands {
+    NONE,
+    READ,
+    CREATE,
+    UPDATE,
+    SHOW_LIST,
+    DELETE,
+    EXIT
+}
